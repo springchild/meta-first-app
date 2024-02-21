c95daa7d888c732b5a86f7d7ed82503051c3af48
springchild <90416491+springchild@users.noreply.github.com>
2024-02-21 17:02:13 -0500

    upd InputComponent.js

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-export default function InputComponent(){
-  const [inputText, setText] = useState('hello');
-  function handleChange(e){
-    setText(e.target.value);
-  }
-  return(
-    <>
-    <input value={inputText} onCange={handleChange}/>
-    <p>You typed: {inputText}</p>
-    <button onClick={()=>setText('hello!')}>
-        Reset
-    </button>
-    </>
-  )
-}
+function InputComponent() {
+    const handler = () => console.log('fourth example')
+    return ( 
+       <div> 
+         <button onClick = {handler} >
+           Click Me!!!!!!!!!
+         </button>
+         <button onClick={ () => console.log('clicked') }> 
+            Click here
+        </button>
+       </div>
+       
+    )
+ }
+ export default InputComponent;
+  
